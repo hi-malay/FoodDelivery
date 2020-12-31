0b14554a090134d87297eaeb4aa0d623df8ef21f
malay3 <malay@goodhealthapp.com>
2020-12-31 16:55:27 +0530

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./screens/home"
+import Restaurant from "./screens/restaurant"
+import OrderDelivery from "./screens/order"
+import { COLORS, icons } from "./constants"
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false,
+          style: {
+            position: 'absolute',
+            left: 0,
+            bottom: 0,
+            right: 0,
+            borderTopWidth: 0,
+            // backgroundColor: "transparent",
+            elevation: 0
+          }
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen name="Restaurant" component={Restaurant}
+          options={{
+            tabBarLabel: 'food',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="food" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen name="OrderDelivery" component={OrderDelivery}
+          options={{
+            tabBarLabel: 'food',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="food" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
